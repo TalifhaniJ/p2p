e55a727e9930734791ab51cdb901d64bc9c25e67
talifhani khomola <127819964+TalifhaniJ@users.noreply.github.com>
2025-02-08 12:57:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on('connect')
 def connect():
